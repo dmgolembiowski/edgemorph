57c4997db0b007aef74a93372a03b417a69d4f95
David Golembiowski <dmgolembiowski@gmail.com>
2020-09-23 23:20:31 -0400

    Creating a new project for EdgeDB SDL/DDL mapping

---
+++ edgemorph/src/lib.rs
@@ -0,0 +1,35 @@
+pub mod datastructures;
+use datastructures::{TypeBuilder, Type};
+
+pub fn new_scalar_type(ident: &str) -> Type {
+    TypeBuilder::default()
+        .ident(ident)
+        .scalar(true)
+        .build()
+        .unwrap()
+}
+
+pub fn _new_object_type(ident: &str) -> Type {
+    TypeBuilder::default()
+        .ident(ident)
+        .scalar(false)
+        .build()
+        .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+    #[test]
+    fn gen_scalar() {
+        let identity = "Double";
+        let scalar = new_scalar_type(&identity);
+        dbg!(&scalar);
+        assert_eq!(scalar.scalar, true);
+    }
+}
