969086ad52bcbe78f7200ec2fb69dbdc06db9cd6
David Golembiowski <david@dgolembiowski.com>
2020-10-09 16:05:58 -0400

    Preparing the API-first specifications for the Rust API code generation.

---
+++ devscripts/codeblocks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+if __name__ == "__main__":
+    if not sys.argv[1]:
+        print("No file supplied")
+        sys.exit(1)
+    else:
+        tokens = []
+        tokens.append("<pre><code>")
+        with open(sys.argv[1], "r") as f:
+            lines = f.readlines()
+        for line in lines:
+            tokens.append(line)
+            tokens.append("<br>")
+        tokens.pop()
+        tokens.append("</code></pre>")
+        print(''.join([t.strip("\n") for t in tokens]))
